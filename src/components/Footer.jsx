fc8a81cb968d82832c69b0e505b49038c6afd509
Andre <ba.dijkman@icloud.com>
2023-03-27 22:16:11 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 const footer = () => {
-  return(
-     <div><h3>Footer</h3></div>
-  )
-  
+  return (
+    <div>
+      <h3>Footer Footer</h3>
+    </div>
+  );
 };
 
 export default footer;
